07c3bd56b919dda0b79c33f782a2a4b93800b47e
Munbin-Lee <3412mb@gmail.com>
2022-06-15 11:39:43 +0900

    Revert "문제점 제거"
    This reverts commit 90ec92aba7dbf0c1128f16e761ed9dbdbe8674d1.

---
+++ src/main/java/twitch4jPackage/TwitchOAuth.java
@@ -0,0 +1,25 @@
+package twitch4jPackage;
+
+import com.github.philippheuer.credentialmanager.CredentialManager;
+import com.github.philippheuer.credentialmanager.CredentialManagerBuilder;
+import com.github.philippheuer.credentialmanager.domain.Credential;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.twitch4j.auth.providers.TwitchIdentityProvider;
+
+public class TwitchOAuth {
+    CredentialManager credentialManager = CredentialManagerBuilder.builder().build();
+    String ClientID;
+    String ClientSecret;
+    String RedirectionURL;
+    OAuth2Credential credential;
+
+    TwitchOAuth(String clientID, String clientSecret, String redirectionURL) {
+        ClientID = clientID;
+        ClientSecret = clientSecret;
+        RedirectionURL = redirectionURL;
+
+        credentialManager.registerIdentityProvider(new TwitchIdentityProvider(ClientID, ClientSecret, RedirectionURL));
+        credential = new OAuth2Credential("twitch", ClientSecret);
+        credentialManager.addCredential("twitch", credential);
+    }
+}
